fefda10489e2e1add376ed94762a5b1ea0f66530
lichking21 <kazakovaman@gmail.com>
2026-03-23 14:23:32 +0100

    (new) User model

---
+++ Domain/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain;
+
+public class User
+{
+    public int ID { get; private set;}
+    public string Name { get; private set;}
+
+    public User(int id, string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("User name can't be empty");
+
+        ID = id;
+        Name = name;
+    }
+}
