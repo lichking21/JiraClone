1142672e95b7e95a78641c2d3dd03be17f4da51c
lichking21 <kazakovaman@gmail.com>
2026-03-23 21:09:05 +0100

    (new) implementation for CompleteTask() method

--- Application/UseCases.cs
+++ Application/UseCases.cs
@@ -2,8 +2,19 @@ namespace Application;
 
 public class UseCases
 {
-    public void CompleteTask()
+    private readonly ITaskRepository _repository;
+
+    public UseCases(ITaskRepository repository)
     {
-        
+        _repository = repository;
+    }
+
+    public void CompleteTask(int id)
+    {
+        var task = _repository.Get(id);
+
+        task.SetStatus(Domain.TaskStatus.Done);
+
+        _repository.Update(task);
     }   
 }
