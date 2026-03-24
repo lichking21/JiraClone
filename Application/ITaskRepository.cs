04d72647b8fbfb756b6737aa92dd6b5830888e23
lichking21 <kazakovaman@gmail.com>
2026-03-24 16:26:16 +0100

    code refactoring

--- Application/ITaskRepository.cs
+++ Application/ITaskRepository.cs
@@ -6,5 +6,5 @@ public interface ITaskRepository
      void Add(Domain.Task task);
      void Remove(int id);
      void Update(Domain.Task task);
-     List<Task> GetUserTasks(int userId);
+     List<Domain.Task> GetUserTasks(int userId);
 }
