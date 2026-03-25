83027880a595d7cac4b019e090bfc979dbae4b4a
lichking21 <kazakovaman@gmail.com>
2026-03-25 17:28:18 +0100

    (new) created bootstrapper

---
+++ CLIApi/Bootstraper.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Infrastructure.DbManager;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace CLI;
+
+public static class Bootstraper
+{
+    public static IHost BuildApp()
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+        builder.Configuration
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        ConfigureServices(builder.Services, builder.Configuration);
+
+        return builder.Build();
+    }
+
+    public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(
+            options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+        );
+
+        services.AddScoped<User>();
+        services.AddScoped<UserTask>();
+    }
+}
